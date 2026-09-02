072b1911c4f3f4046dc3c351192bca9a5417956a
Cristhian Mejias Serrano <cristhianmejiasserrano@gmail.com>
2026-09-01 21:28:13 -0500

    feat: initial database connection and schemas management file created

---
+++ storage/db.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+from psycopg2 import connect
+
+
+def connect_to_db():
+    load_dotenv()
+    conn = connect(
+        database=os.getenv("DB_NAME"),
+        user="postgres",
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST", "localhost"),
+        port="5432",
+    )
+    cursor = conn.cursor()
+    return conn, cursor
