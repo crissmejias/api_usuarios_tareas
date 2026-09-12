3ec0c8c99a9fa356a6f15688d9625ccc1936e8dc
Cristhian Mejias Serrano <cristhianmejiasserrano@gmail.com>
2026-09-11 22:27:23 -0500

    fix: include missing error handlers

--- utils/handlers.py
+++ utils/handlers.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from jwt import ExpiredSignatureError, InvalidTokenError
 from psycopg2 import errors
+from werkzeug import exceptions
 
 
 def register_error_handlers(app):
@@ -23,3 +24,9 @@ def register_error_handlers(app):
     @app.errorhandler(ExpiredSignatureError)
     def handle_jwt_expired_token_error(error):
         return jsonify({"code": 401, "message": "Expired token!", "data": None}), 401
+    @app.errorhandler(exceptions.HTTPException)
+    def handle_http_exception(error):
+        return jsonify({"code": int(error.code), "message": error.description , "data": None}), error.code
+    @app.errorhandler(Exception)
+    def handle_general_exception(error):
+        return jsonify({"code": 500, "message": "Something went wrong!", "data": None}), 500
